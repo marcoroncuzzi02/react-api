c7a3fbe139d6fb3cc289e7724a66d37ed14a526e
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-03-03 22:28:53 +0100

    ADD structure but have error

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors');
+const postRouter = require('./routers/postRouter');
+const endPointMiddleware = require('./middlewares/endPointMiddleware')
+const errorHandler = require('./middlewares/errorHandler')
+
+app.use(cors({
+    origin: 'http://localhost:5174' 
+}));
+
+app.use(express.static('./public/imgs'));
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+res.send('QUESTO è IL SERVER DEL BLOG')
+})
+
+app.use("/api/posts", postRouter)
+
+app.use(endPointMiddleware)
+app.use(errorHandler)
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+})
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react"
 
 function App() {
+const [postsList, setPostsList] = useState([])
+
+function fetchPosts() {
+  fetch("http://localhost:3000/api/posts")
+  .then((res) => res.json())
+  .then(setPostsList);
+}
+
+useEffect(fetchPosts,[])
 
   return (
     <>
-      
+      <ul>
+        {postsList.map( (element) =>{
+          return(
+            <li
+            key={element.id}>
+              {element.title}
+            </li>
+          )
+          
+        })}
+      </ul>
     </>
   )
 }
